65e9b090f1237e6ea7e3b84765589e9b2fc8b24b
Globel99 <kap.levente@gmail.com>
2020-06-21 22:21:23 +0200

    .

---
+++ areaRotation.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class areaRotation : MonoBehaviour
+{   
+    Vector3 axis = new Vector3(0, 0, 1);
+    Vector2 player;
+    PolygonCollider2D collider;
+
+    Vector2 playerToArea = new Vector2(0, -2);
+    Vector2 playerToPoint;
+    Vector2[] areaEdges = new Vector2[2];
+
+    float angle;
+
+    void Start()
+    {
+        collider = gameObject.GetComponent<PolygonCollider2D>(); 
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setPlayerPosition(Vector2 _player)
+    {
+        this.player = _player;
+    }
+
+    void setAreaEdges()
+    {
+        for(int i=0;i<2;i++)
+        {
+            areaEdges[i] = transform.TransformPoint(collider.points[i+1]);
+        }
+    }
+
+    public void Rotate(Vector2 _point)
+    {
+        //régi vektor a player és area között
+        playerToArea = new Vector2(transform.position.x - player.x, transform.position.y - player.y);
+        
+        //új vektor a player és az input között, ide fordul az area
+        playerToPoint  = new Vector2(_point.x - player.x, _point.y - player.y);
+        
+        //régi - új vektor szög
+        angle = Vector2.SignedAngle(playerToArea, playerToPoint);
+
+        //area forgatás
+        transform.RotateAround(player, axis, angle);
+        Debug.DrawLine(player, player + playerToArea, Color.red);
+
+        //edge forgatás
+        setAreaEdges();
+        DrawLinesFromPlayer(areaEdges, Color.red);
+    }
+
+    void DrawLinesFromPlayer(Vector2[] targets, Color color)
+    {
+        foreach(Vector2 target in targets)
+        {
+            Debug.DrawLine(player, target, color);
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    private Vector2 playerPos;
+    private Vector2 areaPos;
+
+    public GameObject area;
+    public GameObject player;
+    public GameObject button;
+
+    bool isAreaRotated = false;
+    bool isMouseButtonDown = false; 
+
+
+    void Start()
+    {
+        Debug.Log("Main Events - starting");
+
+        area = GameObject.Find("Area");
+        player = GameObject.Find("Player");
+        button = GameObject.Find("Button");
+        button.gameObject.SetActive(false);
+
+        playerPos = player.GetComponent<Transform>().position;
+        
+        //körív beállítása a játékoshoz viszonyítva
+        //area = körív
+        areaPos = playerPos;
+        areaPos.y -= 2; 
+        area.GetComponent<Transform>().position = areaPos;
+    }
+
+    void Update()
+    {
+        //lenyomás / tartás
+        if(Input.GetMouseButtonDown(0) || isMouseButtonDown)
+        {
+            Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 inputWorldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+            area.GetComponent<areaRotation>().setPlayerPosition(playerPos);
+            area.GetComponent<areaRotation>().Rotate(inputWorldPosition);
+
+            isMouseButtonDown = true;
+            isAreaRotated = true;
+        }
+
+        //elengedés
+        if(Input.GetMouseButtonUp(0))
+        {
+            isMouseButtonDown = false;
+        }
+    }
+}
